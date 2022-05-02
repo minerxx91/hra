05d2af6b9f93c37fa17d9b628f009942a63540ce
minerxx91 <82457588+minerxx91@users.noreply.github.com>
2022-05-02 20:31:35 +0200

    otacanie

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Camera kamera;
     [SerializeField] float SPEED = 10f;
+    [SerializeField] float ROTATIONSPEED = 300f;
     Vector3 move;
+    Vector3 kameraRotation;
+    Vector3 rotate;
     CharacterController characterController;
     void Start()
     {
@@ -19,12 +22,25 @@ public class Player : MonoBehaviour
         move.x = Input.GetAxis("Horizontal");
         move.z = Input.GetAxis("Vertical");
 
-        if ( !characterController.isGrounded)
+        float x = Input.GetAxis("Mouse Y");
+        float y = Input.GetAxis("Mouse X");
+
+        kameraRotation = new Vector3(x, 0, 0);
+        rotate = new Vector3(0, y, 0);
+        transform.Rotate(rotate * Time.deltaTime * ROTATIONSPEED);
+        if (kamera.transform.eulerAngles.x > 45f && kamera.transform.eulerAngles.x < 270f)
+        {
+            kameraRotation = new Vector3(kamera.transform.eulerAngles.x - 45f, 0, 0);
+        }
+
+        kamera.transform.eulerAngles -= kameraRotation;
+
+        move = transform.TransformDirection(move);
+        characterController.Move(SPEED * Time.deltaTime * move);
+
+        if (!characterController.isGrounded)
             move.y -= 9.8f * Time.deltaTime;
         else
             move.y = 0f;
-
-        move = transform.TransformDirection(move);
-        characterController.Move(move * Time.deltaTime * SPEED);
     }
 }
