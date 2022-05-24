61c9c7f49b00b5033773b102cc57df2b723fd8e5
minerxx91 <82457588+minerxx91@users.noreply.github.com>
2022-05-24 14:26:48 +0200

    swapovanie(pain)

--- Assets/Interactor.cs
+++ Assets/Interactor.cs
@@ -11,7 +11,9 @@ public class Interactor : MonoBehaviour
     //public Dictionary<int, GameObject> items = new Dictionary<int ,GameObject>(3);
     [SerializeField] GameObject hotbarUI;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject models3d;
     GameObject lastCollider;
+    bool empty;
     void Start()
     {
         hotbar = hotbarUI.GetComponent<Hotbar>();
@@ -20,7 +22,6 @@ public class Interactor : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 50f, interactableLayerMask))
         {
             if (hit.collider.GetComponent<Interactable>())
@@ -34,44 +35,44 @@ public class Interactor : MonoBehaviour
                     hit.collider.gameObject.transform.GetChild(0).gameObject.transform.LookAt(player.transform.position);
                     hit.collider.gameObject.transform.GetChild(0).gameObject.transform.eulerAngles = new Vector3(0f, hit.collider.gameObject.transform.GetChild(0).gameObject.transform.eulerAngles.y, 0f);
                 }
-
-                if (Input.GetMouseButton(0))
+                try
                 {
+                    items[hotbar.activeSlot].transform.GetChild(0).gameObject.SetActive(false);
+                }
+                catch { }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+
                     if (hit.collider.gameObject.name == "Pick-Up old" && items[hotbar.activeSlot].tag == "Jerry Can")
                     {
                         hit.collider.gameObject.SetActive(false);
                         onInteract.Invoke();
                         items[hotbar.activeSlot].gameObject.SetActive(false);
-                        /*foreach (GameObject item in items)
-                        {
-                            print(item.gameObject.name);
-                            if (item.gameObject.tag == "Jerry Can")
-                            {
-                                item.gameObject.SetActive(false);
-                            }
-                        }*/
                     }
-
                     if (hit.collider.gameObject.tag == "Jerry Can")
                     {
+                        items[hotbar.activeSlot].gameObject.transform.SetParent(models3d.transform, true);
+                        items[hotbar.activeSlot].gameObject.transform.position = hit.collider.gameObject.transform.position;
+                        items[hotbar.activeSlot].gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                         onInteract.Invoke();
                         hit.collider.gameObject.transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
                         hit.collider.gameObject.transform.localPosition = new Vector3(0f, 1.22f, 0.29f);
-                        items[hotbar.activeSlot].gameObject.transform.parent = null;
-                        items[hotbar.activeSlot].gameObject.transform.position = new Vector3(hit.collider.transform.position.x, 0f, hit.collider.transform.position.z);
-                        items[hotbar.activeSlot].gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                     }
                     else if (hit.collider.gameObject.tag == "Spray")
                     {
-                        onInteract.Invoke();
-                        hit.collider.gameObject.transform.localPosition = new Vector3(0f, -3.356f, -10.303f);
-                        hit.collider.gameObject.transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
-                        items[hotbar.activeSlot].gameObject.transform.parent = null;
-                        items[hotbar.activeSlot].gameObject.transform.position = new Vector3(hit.collider.transform.position.x, 0f, hit.collider.transform.position.z);
+                        items[hotbar.activeSlot].gameObject.transform.SetParent(models3d.transform, true);
+                        items[hotbar.activeSlot].gameObject.transform.position = hit.collider.gameObject.transform.position;
                         items[hotbar.activeSlot].gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                        onInteract.Invoke();
+                        hit.collider.gameObject.transform.localPosition = new Vector3(0, 1.12f, 0.039f);
+                        hit.collider.gameObject.transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
                     }
                     else if (hit.collider.gameObject.tag == "Bat")
                     {
+                        items[hotbar.activeSlot].gameObject.transform.SetParent(models3d.transform, true);
+                        items[hotbar.activeSlot].gameObject.transform.position = hit.collider.gameObject.transform.position;
+                        items[hotbar.activeSlot].gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                         onInteract.Invoke();
                         hit.transform.parent.localPosition = new Vector3(0.318f, 0.256f, -0.019f);
                         hit.transform.parent.localRotation = Quaternion.Euler(0f, -130f, 0f);
